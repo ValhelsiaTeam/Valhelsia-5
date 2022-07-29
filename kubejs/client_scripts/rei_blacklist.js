75387a2a654a08ec9ba4fc59741d71c32d887d8e
Khytwel <khytwel@gmail.com>
2022-07-29 15:18:47 +0200

    Update rei_blacklist.js

--- kubejs/client_scripts/rei_blacklist.js
+++ kubejs/client_scripts/rei_blacklist.js
@@ -1,11 +1,85 @@
 // priority: 0
 
+//   __   ___   _    _  _ ___ _    ___ ___   _       ___ 
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\     | __|
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \    |__ \
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   |___/
+//
+
 onEvent('rei.hide.items', event => {
   event.hide([
+
+    // Minecraft
     'minecraft:barrier',
     'minecraft:jigsaw',
     'minecraft:light',
     'minecraft:structure_block',
     'minecraft:structure_void',
+
+    // Biomes O' Plenty
+    'biomesoplenty:glowshroom_block',
+    'biomesoplenty:glowing_moss_block',
+    'biomesoplenty:glowing_moss_carpet',
+    'biomesoplenty:glowworm_silk',
+    'biomesoplenty:glowworm_silk_strand',
+    'biomesoplenty:glowshroom',
+    'biomesoplenty:potted_origin_sapling',
+    'biomesoplenty:potted_flowering_oak_sapling',
+    'biomesoplenty:potted_rainbow_birch_sapling',
+    'biomesoplenty:potted_yellow_autumn_sapling',
+    'biomesoplenty:potted_orange_autumn_sapling',
+    'biomesoplenty:potted_maple_sapling',
+    'biomesoplenty:potted_fir_sapling',
+    'biomesoplenty:potted_redwood_sapling',
+    'biomesoplenty:potted_white_cherry_sapling',
+    'biomesoplenty:potted_pink_cherry_sapling',
+    'biomesoplenty:potted_mahogany_sapling',
+    'biomesoplenty:potted_jacaranda_sapling',
+    'biomesoplenty:potted_palm_sapling',
+    'biomesoplenty:potted_willow_sapling',
+    'biomesoplenty:potted_dead_sapling',
+    'biomesoplenty:potted_magic_sapling',
+    'biomesoplenty:potted_umbran_sapling',
+    'biomesoplenty:potted_hellbark_sapling',
+    'biomesoplenty:potted_rose',
+    'biomesoplenty:potted_violet',
+    'biomesoplenty:potted_lavender',
+    'biomesoplenty:potted_wildflower',
+    'biomesoplenty:potted_orange_cosmos',
+    'biomesoplenty:potted_pink_daffodil',
+    'biomesoplenty:potted_pink_hibiscus',
+    'biomesoplenty:potted_glowflower',
+    'biomesoplenty:potted_wilted_lily',
+    'biomesoplenty:potted_burning_blossom',
+    'biomesoplenty:potted_sprout',
+    'biomesoplenty:potted_clover',
+    'biomesoplenty:potted_toadstool',
+    'biomesoplenty:potted_glowshroom',
+
+    // Mekanism Additions
+    'mekanismadditions:baby_creeper_spawn_egg',
+    'mekanismadditions:baby_creeper_spawn_egg',
+    'mekanismadditions:baby_wither_skeleton_spawn_egg',
+
+    // Mekanism Tools
+    'mekanismtools:lapis_lazuli_helmet',
+    'mekanismtools:lapis_lazuli_chestplate',
+    'mekanismtools:lapis_lazuli_leggings',
+    'mekanismtools:lapis_lazuli_boots',
+    'mekanismtools:lapis_lazuli_sword',
+    'mekanismtools:lapis_lazuli_pickaxe',
+    'mekanismtools:lapis_lazuli_axe',
+    'mekanismtools:lapis_lazuli_shovel',
+    'mekanismtools:lapis_lazuli_hoe',
+    'mekanismtools:lapis_lazuli_paxel',
+    'mekanismtools:lapis_lazuli_shield',
+
+    // Structure Gel API
+    'structure_gel:red_gel',
+    'structure_gel:blue_gel',
+    'structure_gel:green_gel',
+    'structure_gel:cyan_gel',
+    'structure_gel:orange_gel',
+    'structure_gel:yellow_gel',
   ])
 })
