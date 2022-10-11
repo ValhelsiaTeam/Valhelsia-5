f7e919de6e67aeeb0bc6800ea2946a6a9613bfa3
Khytwel <khytwel@gmail.com>
2022-10-12 00:08:23 +0200

    Update shaped.js

--- kubejs/server_scripts/recipes/shaped.js
+++ kubejs/server_scripts/recipes/shaped.js
@@ -150,6 +150,16 @@ ServerEvents.recipes(event => {
   // Farmer's Delight
   shaped3x3('farmersdelight:organic_compost', 'minecolonies:compost');
 
+  // Forbidden & Arcanus
+  event.shaped('forbidden_arcanus:deorum_chain', [
+    'A',
+    'B',
+    'A'
+  ], {
+    A: 'forbidden_arcanus:deorum_nugget',
+    B: 'forbidden_arcanus:deorum_ingot'
+  });
+
   // Immersive Engineering
   shaped3x3('immersiveengineering:storage_silver', '#forge:ingots/silver');
 
