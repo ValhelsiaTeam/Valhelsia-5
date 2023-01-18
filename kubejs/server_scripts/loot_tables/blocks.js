622ceab59923e4556a39fd4abe0c58600395d3d5
Khytwel <khytwel@gmail.com>
2023-01-18 11:38:12 +0100

    Change Darker Depths Ash Block loot table to drop Supplementaries Ash instead.

---
+++ kubejs/server_scripts/loot_tables/blocks.js
@@ -0,0 +1,24 @@
+// priority: 10
+//   __   ___   _    _  _ ___ _    ___ ___   _       ____ 
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\     | ___|
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \    |___ \
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   |____/
+//
+
+/**
+ * @file Block loot table injection / replacement.
+ * 
+ * @copyright Valhelsia Inc 2022
+ */
+
+/**
+ * Block Loot Table Event Handler
+ */
+ServerEvents.blockLootTables(event => {
+  event.addBlock('darkerdepths:ash_block', table => {
+    table.addPool(pool => {
+      pool.rolls = {min: 3, max: 5}
+      pool.addItem('supplementaries:ash')
+    })
+  })
+})
