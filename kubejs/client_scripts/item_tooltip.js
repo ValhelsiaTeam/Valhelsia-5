0abc6339ca4fa74604698ac03e725e2cc6d9e2be
Khytwel <khytwel@gmail.com>
2023-01-18 14:30:31 +0100

    Add tooltip information about how to get Jade.

---
+++ kubejs/client_scripts/item_tooltip.js
@@ -0,0 +1,34 @@
+// priority: 0
+
+//   __   ___   _    _  _ ___ _    ___ ___   _       ___ 
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\     | __|
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \    |__ \
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   |___/
+//
+
+/**
+ * @file Add or modify item tooltips for Valhelsia 5.
+ * 
+ * Includes a convenience function for adding translated tooltips,
+ * as all tooltips included in Valhelsia packs should use language entries
+ * to ensure translations can be made via CrowdIn.
+ * 
+ * @copyright Valhelsia Inc 2023
+ */
+
+/**
+ * Event handler for adding / adjusting item tooltips.
+ */
+ItemEvents.tooltip(event => {
+
+  /**
+   * Adds a translated tooltip to an item (using default colour).
+   * @param {string} itemID The ID of the item to add the tooltip to.
+   * @param {string} textID The language entry for the tooltip.
+   */
+  const addTooltip = (itemID, textID) => {
+    event.add(itemID, Text.translate(textID));
+  };
+
+  addTooltip('sullysmod:polished_jade', 'valhelsia.tooltip.how_get_jade');
+})
