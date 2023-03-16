410fce87d6038bff01f404d2e6ed05f2a569abe7
Vaelzan <vaelzan@evilcult.net>
2023-03-16 17:54:13 +1100

    Add some additional item tags.

--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -18,45 +18,66 @@
  * Item Tag Event Handler
  */
 ServerEvents.tags('item', event => {
+
+  // ----- Valhelsia Tags -----
   event.add('valhelsia:ropes', [
     'farmersdelight:rope',
-    'supplementaries:rope'
+    'immersiveengineering:wirecoil_structure_rope',
+    'supplementaries:rope',
   ]);
 
   event.add('valhelsia:ash', [
     'supplementaries:ash',
-    'darkerdepths:ash'
+    'darkerdepths:ash',
   ]);
 
   event.add('valhelsia:rose_quartz', [
     'cave_enhancements:rose_quartz',
-    'biomesoplenty:rose_quartz_shard'
+    'create:rose_quartz',
+    'biomesoplenty:rose_quartz_shard',
   ]);
 
+  // ----- Ores -----
   event.add('forge:ores', [
     'darkerdepths:aridrock_silver_ore',
-    'darkerdepths:limestone_silver_ore'
+    'darkerdepths:limestone_silver_ore',
   ]);
 
   event.add('forge:ores/silver', [
     'darkerdepths:aridrock_silver_ore',
-    'darkerdepths:limestone_silver_ore'
+    'darkerdepths:limestone_silver_ore',
   ]);
 
+  // ----- Ingots -----
   event.add('forge:ingots', [
-    'moreminecarts:silica_steel'
+    'moreminecarts:silica_steel',
   ]);
 
   event.add('forge:ingots/silica_steel', [
-    'moreminecarts:silica_steel'
+    'moreminecarts:silica_steel',
   ]);
 
+  // ----- Storage Blocks -----
   event.add('forge:storage_blocks', [
-    'moreminecarts:silica_steel_block'
+    'moreminecarts:silica_steel_block',
   ]);
 
   event.add('forge:storage_blocks/silica_steel', [
-    'moreminecarts:silica_steel_block'
+    'moreminecarts:silica_steel_block',
+  ]);
+
+  // -----Dusts -----
+  event.add('forge:dusts/pixie', [
+    'botania:pixie_dust',
+  ]);
+
+  event.add('forge:dusts/warp', [
+    'waystones:warp_dust',
+  ]);
+
+  event.add('forge:dusts', [
+    'botania:pixie_dust',
+    'waystones:warp_dust',
   ]);
 
 });
