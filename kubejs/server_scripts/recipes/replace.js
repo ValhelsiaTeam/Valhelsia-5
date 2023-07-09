7295cd3991b79cbe5d573bacd642388aac443c57
Vaelzan <vaelzan@evilcult.net>
2023-07-10 06:24:56 +1000

    Add Galosphere Silver to Output Unification.

--- kubejs/server_scripts/recipes/replace.js
+++ kubejs/server_scripts/recipes/replace.js
@@ -75,6 +75,7 @@ ServerEvents.recipes(event => {
   // Note: Priority for metals is Create > Mekanism > Immersive Engineering > Darker Depths
 
   // Nuggets
+  replaceOutput('galosphere:silver_nugget', 'immersiveengineering:nugget_silver');
   replaceOutput('immersiveengineering:nugget_copper', 'create:copper_nugget');
   replaceOutput('immersiveengineering:nugget_lead', 'mekanism:nugget_lead');
   replaceOutput('immersiveengineering:nugget_steel', 'mekanism:nugget_steel');
@@ -82,18 +83,21 @@ ServerEvents.recipes(event => {
 
   // Ingots
   replaceOutput('darkerdepths:silver_ingot', 'immersiveengineering:ingot_silver');
+  replaceOutput('galosphere:silver_ingot', 'immersiveengineering:ingot_silver');
   replaceOutput('immersiveengineering:ingot_lead', 'mekanism:ingot_lead');
   replaceOutput('immersiveengineering:ingot_steel', 'mekanism:ingot_steel');
   replaceOutput('immersiveengineering:ingot_uranium', 'mekanism:ingot_uranium');
 
   // Raw Ore
   replaceOutput('darkerdepths:raw_silver', 'immersiveengineering:raw_silver');
+  replaceOutput('galosphere:raw_silver', 'immersiveengineering:raw_silver');
   replaceOutput('immersiveengineering:raw_lead', 'mekanism:raw_lead');
   replaceOutput('immersiveengineering:raw_uranium', 'mekanism:raw_uranium');
 
   // Storage Blocks
   replaceOutput('darkerdepths:raw_silver_block', 'immersiveengineering:raw_block_silver');
   replaceOutput('darkerdepths:silver_block', 'immersiveengineering:storage_silver');
+  replaceOutput('galosphere:silver_block', 'immersiveengineering:block_silver');
   replaceOutput('immersiveengineering:storage_lead', 'mekanism:block_lead');
   replaceOutput('immersiveengineering:raw_block_lead', 'mekanism:block_raw_lead');
   replaceOutput('immersiveengineering:raw_block_uranium', 'mekanism:block_raw_uranium');
