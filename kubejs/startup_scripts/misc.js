26e914f25c9f474a01b63e782838a7bb8ee04c9e
Vaelzan <vaelzan@evilcult.net>
2023-09-16 06:54:55 +1000

    Fix some mod display names.

---
+++ kubejs/startup_scripts/misc.js
@@ -0,0 +1,16 @@
+// priority: 90
+//   __   ___   _    _  _ ___ _    ___ ___   _       ____ 
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\     | ___|
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \    |___ \
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   |____/
+//
+
+/**
+ * @file Misc startup script contents.
+ * 
+ * @copyright Valhelsia Inc 2023
+ */
+
+Platform.mods.kubejs.name = 'Valhelsia';
+Platform.mods.abnormals_delight.name = 'Abnormals\' Delight';
+Platform.mods.galosphere_delight.name = 'Galosphere Delight';
