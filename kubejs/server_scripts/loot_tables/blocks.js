63242df98a2fb2cff64c28401383f4174cdc431d
Vaelzan <vaelzan@evilcult.net>
2023-09-16 07:07:33 +1000

    Allow ash blocks to be silk touched.

--- kubejs/server_scripts/loot_tables/blocks.js
+++ kubejs/server_scripts/loot_tables/blocks.js
@@ -11,14 +11,39 @@
  * @copyright Valhelsia Inc 2023
  */
 
+const SILK_TOUCH_CONDITION = {
+  condition: 'minecraft:match_tool',
+  predicate: {
+    enchantments: [
+      {
+        enchantment: 'minecraft:silk_touch',
+        levels: {
+          min: 1
+        }
+      }
+    ]
+  }
+};
+
 /**
  * Block Loot Table Event Handler
  */
 ServerEvents.blockLootTables(event => {
+
   event.addBlock('darkerdepths:ash_block', table => {
+    // With Silk Touch
     table.addPool(pool => {
-      pool.rolls = 4
-      pool.addItem('supplementaries:ash')
+      pool.rolls = 1;
+      pool.addItem('darkerdepths:ash_block');
+      pool.addCondition(SILK_TOUCH_CONDITION);
+    });
+
+    // Without Silk Touch
+    table.addPool(pool => {
+      pool.rolls = 4;
+      pool.addItem('supplementaries:ash');
+      pool.addCondition({condition: 'minecraft:inverted', term: SILK_TOUCH_CONDITION});
     });
   });
+  
 });
