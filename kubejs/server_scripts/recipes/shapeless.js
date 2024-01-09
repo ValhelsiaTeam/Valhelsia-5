ced2228c3a8f5b883864bf387f13300e38e34476
Vaelzan <vaelzan@evilcult.net>
2024-01-09 16:34:30 +1100

    Change myalite recipe to use myalite blocks instead of crystals.

--- kubejs/server_scripts/recipes/shapeless.js
+++ kubejs/server_scripts/recipes/shapeless.js
@@ -50,7 +50,7 @@ ServerEvents.recipes(event => {
 
   // Quark
   event.shapeless('2x quark:myalite', ['minecraft:cobblestone', 'minecraft:amethyst_block', 'quark:myalite_crystal']).id(`${ID_PREFIX}myalite`);
-  event.shapeless('quark:dusky_myalite', ['ars_nouveau:sourcestone', 'quark:myalite_crystal']).id(`${ID_PREFIX}dusky_myalite`);
+  event.shapeless('quark:dusky_myalite', ['ars_nouveau:sourcestone', 'quark:myalite']).id(`${ID_PREFIX}dusky_myalite`);
 
   // Supplementaries
   event.shapeless('supplementaries:antique_ink', ['minecraft:ink_sac', 'minecraft:glass_bottle', 'valhelsia:ash']).id(`${ID_PREFIX}antique_ink`);
