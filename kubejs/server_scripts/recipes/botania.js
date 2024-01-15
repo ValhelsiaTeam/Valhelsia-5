0557f86597f5c028f9278cc0a91cb703c1012d6d
Vaelzan <vaelzan@evilcult.net>
2024-01-15 18:54:49 +1100

    Add Mars Stone support to the Orechid.

--- kubejs/server_scripts/recipes/botania.js
+++ kubejs/server_scripts/recipes/botania.js
@@ -166,10 +166,17 @@ ServerEvents.recipes(event => {
   event.recipes.botania.orechid('enlightened_end:malachite_ore', 'minecraft:end_stone', 150);
 
   // Orechid - Modded Stone
+  // Ad Astra Moon
   event.recipes.botania.orechid('ad_astra:moon_cheese_ore', 'ad_astra:moon_stone', 320);
   event.recipes.botania.orechid('ad_astra:moon_desh_ore', 'ad_astra:moon_stone', 600);
   event.recipes.botania.orechid('ad_astra:moon_iron_ore', 'ad_astra:moon_stone', 1080);
   event.recipes.botania.orechid('ad_astra:moon_ice_shard_ore', 'ad_astra:moon_stone', 850);
+
+  // Ad Astra Mars
+  event.recipes.botania.orechid('ad_astra:mars_iron_ore', 'ad_astra:mars_stone', 1350);
+  event.recipes.botania.orechid('ad_astra:mars_ostrum_ore', 'ad_astra:mars_stone', 530);
+  event.recipes.botania.orechid('ad_astra:mars_diamond_ore', 'ad_astra:mars_stone', 150);
+  event.recipes.botania.orechid('ad_astra:mars_ice_shard_ore', 'ad_astra:mars_stone', 850);
 
   // Marimorphosis
   // event.recipes.botania.marimorphosis('output_here', 'input_here', 1, ['plains'], 10);
