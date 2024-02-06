066c0f2fa24a75dd7df9489b12ac6d45f4071a9b
Vaelzan <vaelzan@evilcult.net>
2024-02-07 07:23:07 +1100

    Clean up entity loot table script.

--- kubejs/server_scripts/loot_tables/entities.js
+++ kubejs/server_scripts/loot_tables/entities.js
@@ -7,63 +7,78 @@
 
 /**
  * @file Entity loot table injection / replacement.
- * @copyright Valhelsia Inc 2022-2023
+ * @copyright Valhelsia Inc 2022-2024
  */
 
 /**
  * Entity Loot Table Event Handler
  */
 ServerEvents.entityLootTables(event => {
-  // Add a chance for some mobs to drop additonal loot, as a bonus incentive to farm them.
-
-  // Wraith
-  event.modifyEntity('quark:wraith', table => {
-    table.addPool(pool => {
-      pool.rolls = 1;
-      pool.addItem('mekanism:ingot_lead');
-      pool.killedByPlayer();
-      pool.addCondition({"condition": "minecraft:random_chance_with_looting", "chance": 0.11, "looting_multiplier": 0.02});
-    });
-  });
-
-  // Stray
-  event.modifyEntity('minecraft:stray', table => {
-    table.addPool(pool => {
-      pool.rolls = 1;
-      pool.addItem('immersiveengineering:ingot_silver');
-      pool.killedByPlayer();
-      pool.addCondition({"condition": "minecraft:random_chance_with_looting", "chance": 0.11, "looting_multiplier": 0.02});
-    });
-  });
-
-  // Husk
+  // Minecraft - Husk
   event.modifyEntity('minecraft:husk', table => {
     table.addPool(pool => {
       pool.rolls = 1;
       pool.addItem('mekanism:ingot_tin');
       pool.killedByPlayer();
-      pool.addCondition({"condition": "minecraft:random_chance_with_looting", "chance": 0.11, "looting_multiplier": 0.02});
+      pool.randomChanceWithLooting(0.11, 0.02);
     });
   });
-
-  // Frostbitten
-  event.modifyEntity('rottencreatures:frostbitten', table => {
+  
+  // Minecraft - Stray
+  event.modifyEntity('minecraft:stray', table => {
     table.addPool(pool => {
       pool.rolls = 1;
-      pool.addItem('create:zinc_ingot');
+      pool.addItem('immersiveengineering:ingot_silver');
       pool.killedByPlayer();
-      pool.addCondition({"condition": "minecraft:random_chance_with_looting", "chance": 0.11, "looting_multiplier": 0.02});
+      pool.randomChanceWithLooting(0.11, 0.02);
     });
   });
 
-  // Burned
+  // Quark - Wraith
+  event.modifyEntity('quark:wraith', table => {
+    table.addPool(pool => {
+      pool.rolls = 1;
+      pool.addItem('mekanism:ingot_lead');
+      pool.killedByPlayer();
+      pool.randomChanceWithLooting(0.11, 0.02);
+    });
+  });
+
+  // Rotten Creatures - Burned
   event.modifyEntity('rottencreatures:burned', table => {
     table.addPool(pool => {
       pool.rolls = 1;
       pool.addItem('immersiveengineering:ingot_nickel');
       pool.killedByPlayer();
-      pool.addCondition({"condition": "minecraft:random_chance_with_looting", "chance": 0.11, "looting_multiplier": 0.02});
+      pool.randomChanceWithLooting(0.11, 0.02);
     });
   });
 
+  // Rotten Creatures - Frostbitten
+  event.modifyEntity('rottencreatures:frostbitten', table => {
+    table.addPool(pool => {
+      pool.rolls = 1;
+      pool.addItem('create:zinc_ingot');
+      pool.killedByPlayer();
+      pool.randomChanceWithLooting(0.11, 0.02);
+    });
+  });
+
+  // Twilight Forest - Lich
+  event.modifyEntity('twilightforest:lich', table => {
+    table.addPool(pool => {
+      pool.rolls = 1;
+      pool.addItem('irons_spellbooks:arcane_essence', 1, [0, 4]).lootingEnchant([0, 1], 1);
+    });
+    table.addPool(pool => {
+      pool.rolls = 1;
+      pool.addItem('irons_spellbooks:scroll').addFunction({
+          "function": "irons_spellbooks:randomize_spell",
+          "quality": {
+            "min": 0.0,
+            "max": 0.3
+          }
+        }).randomChanceWithLooting(0.25, 0.05);
+    });
+  });
 });
